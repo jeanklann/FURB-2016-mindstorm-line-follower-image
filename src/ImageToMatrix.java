c9ec3635ba27e076cf548d6078b053c5617218b3
Jean Carlos Klann <jeanklann@hotmail.com>
2016-07-02 12:54:18 -0300

    resumi

--- src/ImageToMatrix.java
+++ src/ImageToMatrix.java
@@ -12,40 +12,27 @@ public class ImageToMatrix {
 		if(file.canRead()){
 			BufferedImage img = ImageIO.read(file);
 			int[][] matrix = new int[img.getWidth()][img.getHeight()];
-			int rgb;
-			int r;
-			int g;
-			int b;
-			int value = 0;
 			System.out.println("{");
 			for(int y = 0; y < img.getHeight(); y++){
 				System.out.print("{");
 				for(int x = 0; x < img.getWidth(); x++){
 					if(x>0) System.out.print(",");
-					
-					rgb = img.getRGB(x, y);
-					r = (rgb >> 16) & 0xFF;
-					g = (rgb >> 8) & 0xFF;
-					b = rgb & 0xFF;
-					
+					int rgb = img.getRGB(x, y);
+					int r = (rgb >> 16) & 0xFF;
+					int g = (rgb >> 8) & 0xFF;
+					int b = rgb & 0xFF;
 					if(	r > contrast && 
 						g > contrast && 
 						b > contrast){
-						value = 0;
+						matrix[x][y] = 0;
 					} else {
-						value = 1;
+						matrix[x][y] = 1;
 					}
-					
-					matrix[x][y] = value;
-					System.out.print(value);
-					
+					System.out.print(matrix[x][y]);
 				}
 				System.out.println("},");
 			}
 			System.out.println("}");
-			
-			//System.out.println(matrix);
-			
 		} else {
 			System.out.println("O arquivo não pode ser lido.");
 		}
